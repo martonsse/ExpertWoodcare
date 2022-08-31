6aed1084d9798175e3a837f8be85e96972a0372e
martonsse <107170181+martonsse@users.noreply.github.com>
2022-08-31 10:55:16 -0500

    Update csvToXmlSSE.py

--- csvToXmlSSE.py
+++ csvToXmlSSE.py
@@ -22,4 +22,6 @@ with open('output.xml', 'w') as e:
                 'city="'+data[x][4]+'" state="'+data[x][5]+'" postal="'+data[x][6]+'" country="'+data[x][7]+'" phone="'+data[x][8]+'" email="'+data[x][9]+'" web="'+data[x][10]+'" '
                 'hours1="'+data[x][11]+'" hours2="'+data[x][12]+'" hours3="'+data[x][13]+'" featured="" features="" />\n')
     e.write('</markers>')
+    
+#reminder that .XML files don't allow certain characters. perhaps if line.contains("&"+"#"+"@").... etc
 
